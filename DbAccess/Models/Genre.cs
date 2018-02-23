269e6c2a3df19626f96c838144b95bb36f8769d2
wiren911 <wirendavid@gmail.com>
2018-02-23 13:35:24 +0100

    Revert "Revert "Project setup""
    This reverts commit 322ea17addc950931e9f6ee1b624cef1d49ae16a.
    
    # Conflicts:
    #	DigitalLibrary.sln
    #	DigitalLibrary/Views/Shared/_Layout.cshtml
    #	DigitalLibrary/Web.config

---
+++ DbAccess/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccess.Models
+{
+    public class Genre
+    {
+        public int GenreID { get; set; }
+        public string GenreName { get; set; }
+        public int BookID { get; set; }
+
+        public virtual Book Books { get; set; }
+    }
+}
---
+++ DbAccess/Models/LibraryInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccess.Models
+{
+    public class LibraryInitializer : DropCreateDatabaseIfModelChanges<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            var books = new List<Book>
+            {
+                new Book {Title ="", ISBN ="" , Year =  },
+                new Book {}
+            };
+            books.ForEach (b => context.Books.Add(b)) ;
+            context.SaveChanges();
+
+            var genres = new List<Genre>
+            {
+                new Genre {},
+                new Genre {}
+            };
+        }
+    }
+}
