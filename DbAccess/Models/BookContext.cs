8dfef6cd607b07a1ef67ceaa4ad48e85ae5d3540
wiren911 <wirendavid@gmail.com>
2018-02-23 09:46:22 +0100

    Project setup

---
+++ DbAccess/Models/BookContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccess.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext() : base("BookContext")
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
